f9d2548fa6e8877aefaa585691c9c1b92eb4c1c4
Sandro Revazishvili <sandro.revazishviliii@gmail.com>
2023-11-26 20:54:16 +0400

    asd

--- src/Api.js
+++ src/Api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 // const BASE_URL = "https://localhost:7239";
-// const BASE_URL = "https://veligroupapi20231125165417.azurewebsites.net";
-const BASE_URL = "https://localhost:7239";
+const BASE_URL = "https://veligroupapi20231125165417.azurewebsites.net";
+// const BASE_URL = "https://localhost:7239";
 export const REPO_NAME = "/veligroup";
 const token = Cookies.get("token");
 //https://veligroupapiapi.azure-api.net
